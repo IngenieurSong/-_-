df9a9072b51c54992bda6b0a62e3df94f630b902
Suin Song <52345271+IngenieurSong@users.noreply.github.com>
2020-09-24 06:16:19 +0900

    05P12
    05P12 과제입니다.

---
+++ 03-05P12-20203086-송수인.ino
@@ -0,0 +1,23 @@
+#define PIN_LED 7
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+  
+  for(int i=0; i<5; i++) {
+    digitalWrite(PIN_LED, 0);
+    delay(200);
+    digitalWrite(PIN_LED, 1);
+    delay(200);
+  }
+
+  while(1) {}
+}
